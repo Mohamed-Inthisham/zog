dbc90806a3a907bacfeff2fea70097dce07cbd6a
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2024-07-12 11:05:20 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav";
 import { 
   Hero,
   CustomerReviews,
@@ -11,7 +12,7 @@ import {
 
 const App = () => (
   <main className="relative">
-    {/* Nav  */}
+    <Nav /> 
     <section className="xl:padding-1 wide:padding-r paddig-b">
       <Hero />
     </section>
